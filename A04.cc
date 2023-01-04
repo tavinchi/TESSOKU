d7e106f67317c789f5ddc10834aaa95f43dd401b
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2023-01-04 11:43:33 +0900

    2進法変換

--- A04.cc
+++ A04.cc
@@ -4,20 +4,10 @@ int main(){
 	int N;
 	cin >>N;
 
-	int x=0,count=0;
-	while(N>0){
-		x=N%2+x*10;
-		N/=2;
-		count++;
+	for(int x=9;x>=0;x--){
+		int wari=(1<<x);
+		cout << (N/wari)%2;
 	}
-
-	N=0;
-	for(int i=0;i<count;i++){
-		N=x%10+N*10;
-		x/=10;
-	}
-
-	printf("%010d",N);
 
 /*		//教科書の解
 	int N;
