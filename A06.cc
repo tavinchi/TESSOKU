62ab452a23dea0b792d82890cb4da04705466bb2
kotaro <cakm1200141@gn.iwasaki.ac.jp>
2023-01-14 14:41:09 +0900

    1次元の累積和

---
+++ A06.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=1;i<=(n);i++)
+
+int main(){
+	int N,Q;
+	cin >>N>>Q;
+	vector<int> a(N+1);
+	rep(i,N) cin >>a[i];
+	vector<int> L(Q+1);
+	vector<int> R(Q+1);
+	rep(i,Q) cin >>L[i]>>R[i];
+
+	vector<int> sum(N+1);
+	sum.at(0)=0;
+	rep(i,N){
+		sum[i]=a[i]+sum[i-1];
+		a[i]=sum[i];
+	}
+
+	rep(i,Q){
+		cout <<a[R[i]]-a[L[i]-1]<<endl;
+	}	
+}
